5627b0442ad828a6c3b82c12076cc35bc9bb41df
Mikhail Nosov <manosov1984@gmail.com>
2022-12-04 21:42:00 +0400

    feat: change BE deploy

--- src/variables/constants.ts
+++ src/variables/constants.ts
@@ -1,5 +1,3 @@
-// export const BACKEND_DOMAIN_FOR_PATH_FILES = 'http://localhost:27017';
-export const BACKEND_DOMAIN_FOR_PATH_FILES = 'https://rs-react-learnwords.herokuapp.com';
+export const BACKEND_DOMAIN_FOR_PATH_FILES = 'https://rslang-be.onrender.com';
 
-export const API_URL = `https://learnenglishwords-app.herokuapp.com`;
-// export const API_URL = `http://localhost:8081`;
+export const API_URL = `https://rslang-be.onrender.com`;
